ddca01c2dd3ab96e80e95b3f5772f1eaaec5b29e
Jesse Freeman <jessefreeman@gmail.com>
2018-02-07 14:40:18 -0500

    Cleaning up required files.

--- Runner/IDisplayTarget.cs
+++ Runner/IDisplayTarget.cs
@@ -1,5 +1,17 @@
-﻿
-using PixelVisionSDK;
+﻿//
+// Copyright (c) Jesse Freeman. All rights reserved.  
+//
+// Licensed under the Microsoft Public License (MS-PL) License. 
+// See LICENSE file in the project root for full license information. 
+//
+// Contributors
+// --------------------------------------------------------
+// This is the official list of Pixel Vision 8 contributors:
+//
+// Jesse Freeman - @JesseFreeman
+// Christer Kaitila - @McFunkypants
+// Pedro Medeiros - @saint11
+// Shawn Rakowski - @shwany
 
 namespace PixelVisionRunner
 {
@@ -8,6 +20,5 @@ namespace PixelVisionRunner
         void ResetResolution(int width, int height);
         void Render();
         void CacheColors();
-//        bool fullScreen { get; }
     }
 }
--- Runner/ITextureFactory.cs
+++ Runner/ITextureFactory.cs
@@ -1,4 +1,19 @@
-﻿namespace PixelVisionRunner
+﻿//
+// Copyright (c) Jesse Freeman. All rights reserved.  
+//
+// Licensed under the Microsoft Public License (MS-PL) License. 
+// See LICENSE file in the project root for full license information. 
+//
+// Contributors
+// --------------------------------------------------------
+// This is the official list of Pixel Vision 8 contributors:
+//
+// Jesse Freeman - @JesseFreeman
+// Christer Kaitila - @McFunkypants
+// Pedro Medeiros - @saint11
+// Shawn Rakowski - @shwany
+
+namespace PixelVisionRunner
 {
     public interface ITextureFactory
     {
