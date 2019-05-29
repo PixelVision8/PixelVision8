86113d3c1656c51371631a6e3a1268e4e7a5d0ca
Jesse Freeman <jessefreeman@gmail.com>
2019-05-29 07:57:17 -0400

    Add wav conversion to RawAudioData.

--- Runners/PixelVision8/Runner/Data/RawAudioData.cs
+++ Runners/PixelVision8/Runner/Data/RawAudioData.cs
@@ -31,11 +31,7 @@ namespace PixelVision8.Runner.Data
         public int channels { get;}
 
         public int frequency { get; }
-//        public static RawAudioData NewAudioClip(int lengthSamples, int channels, int frequency, bool stream)
-//        {
-//            return new RawAudioData(lengthSamples, channels, frequency);    
-//        }
-        
+
         public RawAudioData(int samples, int channels, int frequency)
         {
             this.samples = samples;
@@ -69,6 +65,151 @@ namespace PixelVision8.Runner.Data
         {
             Array.Resize(ref data, samples);
             this.samples = samples;
+        }
+        
+        // CONVERT TO WAV
+        
+        /// <summary>
+        ///     Returns a ByteArray of the wave in the form of a .wav file, ready to be saved out
+        /// </summary>
+        /// <param name="__sampleRate">Sample rate to generate the .wav data at (44100 or 22050, default 44100)</param>
+        /// <param name="__bitDepth">Bit depth to generate the .wav at (8 or 16, default 16)</param>
+        /// <returns>Wave data (in .wav format) as a byte array</returns>
+        public byte[] GenerateWav()
+        {
+
+            var __sampleRate = 22050u;
+            var __bitDepth = 8u;
+
+            var soundLength = Convert.ToUInt32(samples);
+            if (__bitDepth == 16) soundLength *= 2;
+            if (__sampleRate == 22050) soundLength /= 2;
+
+            var fileSize = 36 + soundLength;
+            var blockAlign = __bitDepth / 8;
+            var bytesPerSec = __sampleRate * blockAlign;
+
+            // The file size is actually 8 bytes more than the fileSize
+            var wav = new byte[fileSize + 8];
+
+            var bytePos = 0;
+
+            // Header
+
+            // Chunk ID "RIFF"
+            writeUintToBytes(wav, ref bytePos, 0x52494646, Endian.BIG_ENDIAN);
+
+            // Chunck Data Size
+            writeUintToBytes(wav, ref bytePos, fileSize, Endian.LITTLE_ENDIAN);
+
+            // RIFF Type "WAVE"
+            writeUintToBytes(wav, ref bytePos, 0x57415645, Endian.BIG_ENDIAN);
+
+            // Format Chunk
+
+            // Chunk ID "fmt "
+            writeUintToBytes(wav, ref bytePos, 0x666D7420, Endian.BIG_ENDIAN);
+
+            // Chunk Data Size
+            writeUintToBytes(wav, ref bytePos, 16, Endian.LITTLE_ENDIAN);
+
+            // Compression Code PCM
+            writeShortToBytes(wav, ref bytePos, 1, Endian.LITTLE_ENDIAN);
+            // Number of channels
+            writeShortToBytes(wav, ref bytePos, 1, Endian.LITTLE_ENDIAN);
+            // Sample rate
+            writeUintToBytes(wav, ref bytePos, __sampleRate, Endian.LITTLE_ENDIAN);
+            // Average bytes per second
+            writeUintToBytes(wav, ref bytePos, bytesPerSec, Endian.LITTLE_ENDIAN);
+            // Block align
+            writeShortToBytes(wav, ref bytePos, (short) blockAlign, Endian.LITTLE_ENDIAN);
+            // Significant bits per sample
+            writeShortToBytes(wav, ref bytePos, (short) __bitDepth, Endian.LITTLE_ENDIAN);
+
+            // Data Chunk
+
+            // Chunk ID "data"
+            writeUintToBytes(wav, ref bytePos, 0x64617461, Endian.BIG_ENDIAN);
+            // Chunk Data Size
+            writeUintToBytes(wav, ref bytePos, soundLength, Endian.LITTLE_ENDIAN);
+
+            // Write data as bytes
+            var sampleCount = 0;
+            var bufferSample = 0f;
+            for (var i = 0; i < data.Length; i++)
+            {
+                bufferSample += data[i];
+                sampleCount++;
+
+                if (sampleCount == 2)
+                {
+                    bufferSample /= sampleCount;
+                    sampleCount = 0;
+
+                        writeBytes(wav, ref bytePos, new[] {(byte) (Math.Round(bufferSample * 127f) + 128)},
+                            Endian.LITTLE_ENDIAN);
+
+                    bufferSample = 0f;
+                }
+            }
+
+            return wav;
+        }
+        
+        /// <summary>
+        ///     Writes a short (Int16) to a byte array.
+        ///     This is an aux function used when creating the WAV data.
+        /// </summary>
+        /// <param name="__bytes"></param>
+        /// <param name="__position"></param>
+        /// <param name="__newShort"></param>
+        /// <param name="__endian"></param>
+        private void writeShortToBytes(byte[] __bytes, ref int __position, short __newShort, Endian __endian)
+        {
+            writeBytes(__bytes, ref __position,
+                new byte[2] {(byte) ((__newShort >> 8) & 0xff), (byte) (__newShort & 0xff)}, __endian);
+        }
+
+        /// <summary>
+        ///     Writes a uint (UInt32) to a byte array.
+        ///     This is an aux function used when creating the WAV data.
+        /// </summary>
+        /// <param name="__bytes"></param>
+        /// <param name="__position"></param>
+        /// <param name="__newUint"></param>
+        /// <param name="__endian"></param>
+        private void writeUintToBytes(byte[] __bytes, ref int __position, uint __newUint, Endian __endian)
+        {
+            writeBytes(__bytes, ref __position,
+                new byte[4]
+                {
+                    (byte) ((__newUint >> 24) & 0xff), (byte) ((__newUint >> 16) & 0xff),
+                    (byte) ((__newUint >> 8) & 0xff), (byte) (__newUint & 0xff)
+                }, __endian);
+        }
+
+        /// <summary>
+        ///     Writes any number of bytes into a byte array, at a given position.
+        ///     This is an aux function used when creating the WAV data.
+        /// </summary>
+        /// <param name="__bytes"></param>
+        /// <param name="__position"></param>
+        /// <param name="__newBytes"></param>
+        /// <param name="__endian"></param>
+        private void writeBytes(byte[] __bytes, ref int __position, byte[] __newBytes, Endian __endian)
+        {
+            // Writes __newBytes to __bytes at position __position, increasing the position depending on the length of __newBytes
+            for (var i = 0; i < __newBytes.Length; i++)
+            {
+                __bytes[__position] = __newBytes[__endian == Endian.BIG_ENDIAN ? i : __newBytes.Length - i - 1];
+                __position++;
+            }
+        }
+
+        private enum Endian
+        {
+            BIG_ENDIAN,
+            LITTLE_ENDIAN
         }
 
     }
--- Runners/PixelVision8/Runner/Exporters/SongExporter.cs
+++ Runners/PixelVision8/Runner/Exporters/SongExporter.cs
@@ -70,11 +70,6 @@ namespace PixelVision8.Runner.Exporters
 
             this.patterns = patterns;
 
-            // Save a reference to the audio factory
-//            this.audioClipFactory = audioClipFactory;
-
-            // Calculate steps
-//            CalculateSteps();
         }
 
         public override void CalculateSteps()
@@ -234,12 +229,12 @@ namespace PixelVision8.Runner.Exporters
         private void CreateSongByteData()
         {
             //TODO need to break this process up in to steps so it doesn't block the runner
-            bytes = Save(fileName, result);
+            bytes = result.GenerateWav();
             currentStep++;
         }
 
         // pre-rendered waveforms - OPTIMIZATION - SLOW SLOW SLOW - FIXME
-        public /* static */ RawAudioData MixdownAudioClips(params RawAudioData[] clips)
+        public RawAudioData MixdownAudioClips(params RawAudioData[] clips)
         {
             if (clips == null || clips.Length == 0) return null;
 
@@ -292,122 +287,6 @@ namespace PixelVision8.Runner.Exporters
             return result;
         }
 
-        #region Save Wav Code
-
-        //  derived from Gregorio Zanon's script
-//  http://forum.unity3d.com/threads/119295-Writing-AudioListener.GetOutputData-to-wav-problem?p=806734&viewfull=1#post806734
-
-
-        private const int HEADER_SIZE = 44;
-
-        public byte[] Save(string filepath, RawAudioData clip)
-        {
-            MemoryStream fileStream;
-
-            using (fileStream = CreateEmpty())
-            {
-                ConvertAndWrite(fileStream, clip);
-
-                WriteHeader(fileStream, clip);
-            }
-
-            return fileStream.GetBuffer(); // TODO: return false if there's a failure saving the file
-        }
-
-        private MemoryStream CreateEmpty()
-        {
-//		var fileStream = new FileStream(filepath, FileMode.Create);
-            var emptyByte = new byte();
-
-            var fileStream = new MemoryStream(emptyByte);
-            for (var i = 0; i < HEADER_SIZE; i++) //preparing the header
-                fileStream.WriteByte(emptyByte);
-
-            return fileStream;
-        }
-
-        private void ConvertAndWrite(MemoryStream fileStream, RawAudioData clip)
-        {
-            var samples = new float[clip.samples];
-
-            clip.GetData(samples);
-
-            var intData = new short[samples.Length];
-            //converting in 2 float[] steps to Int16[], //then Int16[] to Byte[]
-
-            var bytesData = new byte[samples.Length * 2];
-            //bytesData array is twice the size of
-            //dataSource array because a float converted in Int16 is 2 bytes.
-
-            var rescaleFactor = 32767; //to convert float to Int16
-
-            for (var i = 0; i < samples.Length; i++)
-            {
-                intData[i] = (short) (samples[i] * rescaleFactor);
-                var byteArr = new byte[2];
-                byteArr = BitConverter.GetBytes(intData[i]);
-                byteArr.CopyTo(bytesData, i * 2);
-            }
-
-            fileStream.Write(bytesData, 0, bytesData.Length);
-        }
-
-        private void WriteHeader(MemoryStream fileStream, RawAudioData clip)
-        {
-            var hz = clip.frequency;
-            var channels = clip.channels;
-            var samples = clip.samples;
-
-            fileStream.Seek(0, SeekOrigin.Begin);
-
-            var riff = Encoding.UTF8.GetBytes("RIFF");
-            fileStream.Write(riff, 0, 4);
-
-            var chunkSize = BitConverter.GetBytes(fileStream.Length - 8);
-            fileStream.Write(chunkSize, 0, 4);
-
-            var wave = Encoding.UTF8.GetBytes("WAVE");
-            fileStream.Write(wave, 0, 4);
-
-            var fmt = Encoding.UTF8.GetBytes("fmt ");
-            fileStream.Write(fmt, 0, 4);
-
-            var subChunk1 = BitConverter.GetBytes(16);
-            fileStream.Write(subChunk1, 0, 4);
-
-            //UInt16 two = 2; // unused
-            ushort one = 1;
-
-            var audioFormat = BitConverter.GetBytes(one);
-            fileStream.Write(audioFormat, 0, 2);
-
-            var numChannels = BitConverter.GetBytes(channels);
-            fileStream.Write(numChannels, 0, 2);
-
-            var sampleRate = BitConverter.GetBytes(hz);
-            fileStream.Write(sampleRate, 0, 4);
-
-            var byteRate =
-                BitConverter.GetBytes(hz * channels *
-                                      2); // sampleRate * bytesPerSample*number of channels, here 44100*2*2
-            fileStream.Write(byteRate, 0, 4);
-
-            var blockAlign = (ushort) (channels * 2);
-            fileStream.Write(BitConverter.GetBytes(blockAlign), 0, 2);
-
-            ushort bps = 16;
-            var bitsPerSample = BitConverter.GetBytes(bps);
-            fileStream.Write(bitsPerSample, 0, 2);
-
-            var datastring = Encoding.UTF8.GetBytes("data");
-            fileStream.Write(datastring, 0, 4);
-
-            var subChunk2 = BitConverter.GetBytes(samples * channels * 2);
-            fileStream.Write(subChunk2, 0, 4);
-
-//		fileStream.Close();
-        }
-
-        #endregion
+        
     }
 }
